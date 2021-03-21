a47df4df0d7d342f153014045c13c3d96c4554e3
Toms15 <info@tommasocostantini.it>
2021-03-21 18:12:58 +0100

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -80,6 +80,13 @@ $table_prefix  = 'pl_';
 define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
+define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'praxigroup.officine06lab.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
